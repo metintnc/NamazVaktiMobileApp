19baa0aaf5927d6a06e38da2c02f0faca216e9b4
Metin Can Tunç <metinncann56@gmail.com>
2025-11-18 22:50:26 +0300

    Kıble Bulma (Pusula) Modülünün Çekirdek Mantığı Tamamlandı Konum ve Hata Kontrolü ,Kıble Hesaplama ,Sensör Yaşam Döngüsü İşlemleri Tamamlandı

---
+++ hadis/Pusula.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Devices.Sensors;
+
+namespace hadis
+{
+    public class Pusula
+    {
+        private double enlem, boylam , hedefKibleAcisi;
+        const double kabeenlem = 21.4225;
+        const double kabeboylam = 39.8262;
+        private bool _pusulaAktifMi;
+        public event Action<double> AciDegisti;
+        public Pusula()
+        {
+
+        }
+        public async Task KontrolEt()
+        {
+            var konum = await Geolocation.GetLastKnownLocationAsync();
+            if(konum == null)
+            {
+                konum = await Geolocation.GetLocationAsync();
+            }
+            if(konum == null)
+            {
+                return;
+            }
+                enlem = konum.Latitude;
+                boylam = konum.Longitude;
+            AciyiHesapla();
+            PusulaBaslat();
+        }
+        public void AciyiHesapla()
+        {
+            double _enlemRad = enlem * (Math.PI / 180);
+            double _boylamRad = boylam * (Math.PI / 180);
+            double _kabeenlemRad = kabeenlem * (Math.PI / 180);
+            double _kabeboylamRad = kabeboylam * (Math.PI / 180);
+
+            double boylamfark = _kabeboylamRad - _boylamRad;
+
+            double Y = Math.Sin(boylamfark) *Math.Cos(_kabeenlemRad);
+            double X = Math.Cos(_enlemRad) * Math.Sin(_kabeenlemRad) - Math.Sin(_enlemRad) * Math.Cos(_kabeenlemRad) * Math.Cos(boylamfark);
+
+            double aci = Math.Atan2(Y, X) * 180/Math.PI;
+            hedefKibleAcisi = (aci + 360) % 360;
+        }
+
+        public void PusulaBaslat()
+        {
+            if (!Compass.Default.IsSupported || _pusulaAktifMi)
+            {
+                return;
+            }
+            else
+            {
+                Compass.Default.ReadingChanged += PusulaVerisiGeldi;
+                Compass.Default.Start(SensorSpeed.UI);
+                _pusulaAktifMi = true;
+            }
+        }
+        public void PusulaVerisiGeldi(object sender, CompassChangedEventArgs e)
+        {
+            double kuzey = e.Reading.HeadingMagneticNorth;
+            double donusacisi = hedefKibleAcisi - kuzey;
+            donusacisi = (donusacisi + 360) % 360;
+            AciDegisti?.Invoke(donusacisi);
+        }
+        
+        public void PusulaDurdur()
+        {
+            if (!_pusulaAktifMi)
+            {
+                return;
+            }
+            else
+            {
+                Compass.Default.ReadingChanged -= PusulaVerisiGeldi;
+                Compass.Default.Stop();
+                _pusulaAktifMi = false;
+            }
+        }
+
+    }
+}
